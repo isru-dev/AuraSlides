b02848eb9d81fee3acfe056acb28b3344c1e1b71
israel Gezahegn <isrubest18@gmail.com>
2026-06-09 02:53:58 +0300

    intial commit:starting to build AuraSlides

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // 🚀 Injects the ultra-fast v4 compiler!
+  ],
+})
