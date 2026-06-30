14e4e95fc1a20e827acdf935b00a2281a8e4a2bd
israel Gezahegn <isrubest18@gmail.com>
2026-07-01 02:32:34 +0300

    add error handling for  presentations and update presentation not found response

--- server/routes/presentationRoutes.js
+++ server/routes/presentationRoutes.js
@@ -52,4 +52,53 @@ router.get("/:id", protect, async (req, res) => {
     });
   }
 });
+
+router.get("/", protect, async (req, res) => {
+  try {
+    const allPresentation = await Presentation.find({
+      owner: req.user.userId,
+    });
+    if (allPresentation.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No presentations found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      presentations: allPresentations,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+router.put("/:id", protect, async (req, res) => {
+  try {
+    const presentation = await Presentation.findOneAndUpdate({
+      _id: req.params.id,
+      owner: req.user.userId,
+    });
+    if (!presentation) {
+      return res.status(404).json({
+        success: false,
+        message: "Presentation not found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      presentation,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 module.exports = router;
